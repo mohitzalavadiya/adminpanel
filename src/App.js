62d1e0334abc70d651c3a0b2414a1b2a48c220fc
mohitzalavadiya <mzpatel14@gmail.com>
2022-08-04 15:31:56 +0530

    presistor

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import Layout from "./components/layout/Layout";
 import Medicine from "./container/medicine/Medicine";
 import Patient from "./container/patient/Patient";
 import configureStore from "./redux/Store";
-import {Provider} from "react-redux"
+import { Provider } from "react-redux";
 import Counter from "./Counter";
-
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
-  const store = configureStore() 
+  const { store, persistor } = configureStore();
 
   return (
     <Provider store={store}>
-    <Layout>
-      <Switch>
-        <Route path="/medicine" exact component={Medicine} />
-        <Route path="/patient" exact component={Patient} />
-        <Route path = "/counter" exact component={Counter} />
-      </Switch>
-    </Layout>
+      <PersistGate loading={null} persistor={persistor}>
+        <Layout>
+          <Switch>
+            <Route path="/medicine" exact component={Medicine} />
+            <Route path="/patient" exact component={Patient} />
+            <Route path="/counter" exact component={Counter} />
+          </Switch>
+        </Layout>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,11 +1,21 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
 import Rootreducer from ".";
+import Counter from "../Counter";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["counter"],
+};
+const persistedReducer = persistReducer(persistConfig, Rootreducer);
 
 const configureStore = () => {
-    const store = createStore(Rootreducer, applyMiddleware(thunk));
-    
-    return store;
-}
+  const store = createStore(persistedReducer, applyMiddleware(thunk));
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
 
 export default configureStore;
